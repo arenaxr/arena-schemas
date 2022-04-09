9dbe49ea7939da609c72889a32cb52863eb83c3a
mwfarb <mwfarb@andrew.cmu.edu>
2022-04-09 19:09:37 -0400

    resolve ref data prop

--- updateschemas.py
+++ updateschemas.py
@@ -3,12 +3,32 @@ import os
 import sys
 
 output_folder = 'schemas/'
+input_folder = ''
+
+
+def parse_allof_ref(allof_schema):
+    global input_folder
+    new_schema = allof_schema
+    for props in allof_schema['allOf']:
+        for key in props:
+            if key == '$ref':
+                ref = props[key].split('#/')
+                key = ref[1]
+                with open(f'{input_folder}/{ref[0]}') as f:
+                    sub_schema = json.load(f)
+                props = sub_schema
+            if key not in new_schema:
+                new_schema[key] = {}
+            new_schema[key].update(props[key])
+    return new_schema
+
 
 def main():
+    global input_folder, output_folder
     args = sys.argv[1:]
-    print (args)
+    print(args)
     if (len(args) == 0 or not os.path.isdir(args[0])):
-        print ('Supply a valid source schemas path! src=ARENA-core/build')
+        print('Supply a valid source schemas path! src=ARENA-core/build')
         return
 
     input_folder = args[0]
@@ -23,35 +43,30 @@ def main():
 
         # resolve all references
         if 'allOf' in schema:
-            new_schema = schema
-            for props in schema['allOf']:
-                for key in props:
-                    if key == '$ref':
-                        ref = props[key].split('#/')
-                        with open(f'{input_folder}/{ref[0]}') as f:
-                            sub_schema = json.load(f)
-                        props = sub_schema
-                        if ref[1] not in new_schema:
-                            new_schema[ref[1]]={}
-                    new_schema[ref[1]].update(props[ref[1]])
+            new_schema = parse_allof_ref(schema)
             del schema['allOf']
         else:
-            new_schema = schema ## options/program
+            new_schema = schema
+
+        if 'allOf' in new_schema['properties']['data']:
+            new_schema['properties']['data'] = parse_allof_ref(new_schema['properties']['data'])
+            del new_schema['properties']['data']['allOf']
+        else:
+            new_schema['properties']['data'] = new_schema['properties']['data']
 
         # resolve missing definitions
 
         # write this object expanded json schema
         new_obj_json = json.dumps(new_schema, indent=4)
-        file = open(f'{output_folder}{fn[8:]}','w')
+        file = open(f'{output_folder}{fn[8:]}', 'w')
         file.write(new_obj_json)
         file.close()
 
-        #break # for now
+        break # for now
 
-    file = open(f'{output_folder}arena-schema-files.json','w')
+    file = open(f'{output_folder}arena-schema-files.json', 'w')
     file.write(json.dumps(obj_schemas, indent=4))
     file.close()
-
 
     # with open(arena_objects_schema_path) as f:
     #     schema = json.load(f)
@@ -105,6 +120,7 @@ def main():
     # file = open(f'{output_folder}arena-schema-files.json','w')
     # file.write(json.dumps(obj_schemas, indent=4))
     # file.close()
+
 
 '''
     for key in definitions:
